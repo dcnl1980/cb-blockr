0c71bfab9273aec27a1efc7e3df4eb1ecc88a867
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-06-20 14:08:13 -0400

    fix throttling

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 var assert = require('assert')
 var request = require('superagent')
 var async = require('async')
+var throttle = require('throttleme')
 var proxyURL;
 var REQ_THROTTLE = 500
 var POST_REQ_THROTTLE = 1000
@@ -102,39 +103,6 @@ var makePostRequest = throttle(function makePostRequest(uri, form, callback){
 
 function setProxyURL(url) {
   proxyURL = url
-}
-
-// from underscore.js
-function throttle(func, wait, options) {
-  var context, args, result;
-  var timeout = null;
-  var previous = 0;
-  if (!options) options = {};
-  var later = function() {
-    previous = options.leading === false ? 0 : Date.now();
-    timeout = null;
-    result = func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
-  return function() {
-    var now = Date.now();
-    if (!previous && options.leading === false) previous = now;
-    var remaining = wait - (now - previous);
-    context = this;
-    args = arguments;
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      previous = now;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
-    }
-    return result;
-  };
 }
 
 module.exports = {
