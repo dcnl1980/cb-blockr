3cf33e4c6640e5d2a0019350624c9601b96e4e79
Mark Vayngrib <mark@tradle.io>
2017-01-02 08:48:03 +0800

    add .info()

--- src/index.js
+++ src/index.js
@@ -14,12 +14,15 @@ var NETWORKS = {
 function Blockr(network, proxyURL) {
   network = network || 'bitcoin'
   assert(network in NETWORKS, 'Unknown network: ' + network)
-  var BASE_URL = 'https://' + NETWORKS[network] + '.blockr.io/api/v1/'
+  var BASE_URL = this._baseUrl = 'https://' + NETWORKS[network] + '.blockr.io/api/v1/'
 
   // end points
   this.transactions = new Transactions(BASE_URL + 'tx/')
   this.addresses = new Addresses(BASE_URL + 'address/', this.transactions)
   this.blocks = new Blocks(BASE_URL + 'block/', this.transactions)
+  this._urlForPath = function (path) {
+    return BASE_URL + path
+  }
 
   this.network = network
 
@@ -30,6 +33,18 @@ function Blockr(network, proxyURL) {
 Blockr.Addresses = Addresses
 Blockr.Blocks = Blocks
 Blockr.Transactions = Transactions
+Blockr.prototype.info = function (cb) {
+  utils.makeRequest(this._urlForPath('coin/info'), function (err, data) {
+    if (err) return cb(err)
+
+    var block = data.last_block
+    cb(null, {
+      blockHeight: block.nb,
+      timestamp: new Date(block.time_utc).getTime(),
+      txCount: block.nb_txs
+    })
+  })
+}
 
 Blockr.prototype.getNetwork = function() { return this.network }
 Blockr.prototype.getProxyURL = function() { return this.proxyURL }
